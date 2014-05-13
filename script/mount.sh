7d269cf0060b9d6aaa6ca6587d1a457a474108a2
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2014-05-13 15:54:40 +0900

    [Mount] Add skeltone of script for mounting aufs.

---
+++ script/mount.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+watch=0
+machine=0
+while [ $watch -ne 12 ]
+do
+    watch=`expr $watch + 1`
+    monitor_version=""
+    case $watch in
+        [1-5] ) monitor_version="Zabbix2.0";;
+        6     ) monitor_version="Zabbix1.8";;
+        [7-8] ) monitor_version="Zabbix2.2";;
+        ^9|10 ) monitor_version="Nagios3";;
+        1[1-2] ) monitor_version="Nagios4";;
+    esac
+
+    machine=0
+    while [ $machine -ne 5 ]
+    do
+        machine=`expr $machine + 1`
+        monitor_type=""
+        case $machine in
+            1 ) monitor_type="Server";;
+        [2-5] ) monitor_type="Client";;
+        esac
+
+        echo "[${watch}][${machine}][${monitor_version}][${monitor_type}]"
+    done
+done
