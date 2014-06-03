4c5a6a2016981141364b47ccb9031129d196606b
Noriki Nakamura <bynn_chapu@tencho-lab.net>
2014-06-03 11:13:08 +0900

    [Login] Change order of logining.

--- script/for_test/login.sh
+++ script/for_test/login.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-echo "Machine 6"
-curl -X GET -H "Content-Type:application/json-rpc" -d \
-	'{"auth":null,"method":"user.login","id":1,"params":{"user":"admin","password":"zabbix"},"jsonrpc":"2.0"}' \
-	http://10.0.3.61/zabbix/api_jsonrpc.php
-echo ""
-
 echo "Machine 2"
 curl -X GET -H "Content-Type:application/json-rpc" -d \
 	'{"auth":null,"method":"user.login","id":1,"params":{"user":"admin","password":"zabbix"},"jsonrpc":"2.0"}' \
 	http://10.0.3.21/zabbix/api_jsonrpc.php
+echo ""
+
+echo "Machine 6"
+curl -X GET -H "Content-Type:application/json-rpc" -d \
+	'{"auth":null,"method":"user.login","id":1,"params":{"user":"admin","password":"zabbix"},"jsonrpc":"2.0"}' \
+	http://10.0.3.61/zabbix/api_jsonrpc.php
 echo ""
 
 echo "Machine 7"
