6718170f901222a0ce0cff93ae880acec86cd24f
Noriki Nakamura <bynn_chapu@tencho-lab.net>
2014-06-03 11:12:17 +0900

    [Login] Fix messages.

--- script/for_test/login.sh
+++ script/for_test/login.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-echo "for Zabbix 1.8"
+echo "Machine 6"
 curl -X GET -H "Content-Type:application/json-rpc" -d \
 	'{"auth":null,"method":"user.login","id":1,"params":{"user":"admin","password":"zabbix"},"jsonrpc":"2.0"}' \
 	http://10.0.3.61/zabbix/api_jsonrpc.php
 echo ""
 
-echo "for Zabbix 2.0"
+echo "Machine 2"
 curl -X GET -H "Content-Type:application/json-rpc" -d \
 	'{"auth":null,"method":"user.login","id":1,"params":{"user":"admin","password":"zabbix"},"jsonrpc":"2.0"}' \
 	http://10.0.3.21/zabbix/api_jsonrpc.php
 echo ""
 
-echo "for Zabbix 2.2"
+echo "Machine 7"
 curl -X GET -H "Content-Type:application/json-rpc" -d \
 	'{"auth":null,"method":"user.login","id":1,"params":{"user":"Admin","password":"zabbix"},"jsonrpc":"2.0"}' \
 	http://10.0.3.71/zabbix/api_jsonrpc.php
