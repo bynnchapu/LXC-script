e60dca3e8d282bb86bcfb47d751afce03bd6c855
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2014-06-04 13:03:51 +0900

    [getGroup] Fix wrong variable and url.
    TOKEN_3 ->
    TOKEN_6
    
    TOKEN_4 ->
    TOKEN_7
    
    http://10.0.3.21/ ->
    http://10.0.3.61/
    
    http://10.0.3.21/ ->
    http://10.0.3.71/

--- script/for_test/getGroup.sh
+++ script/for_test/getGroup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 TOKEN_2=$1
-TOKEN_3=$2
-TOKEN_4=$3
+TOKEN_6=$2
+TOKEN_7=$3
 
 echo "Machine 2"
 curl -X GET -H "Content-Type:application/json-rpc" -d \
@@ -12,12 +12,12 @@ echo ""
 
 echo "Machine 6"
 curl -X GET -H "Content-Type:application/json-rpc" -d \
-	'{"auth":"'${TOKEN_2}'","method":"hostgroup.get","id":1,"params":{"output":"extend"},"jsonrpc":"2.0"}' \
-	http://10.0.3.21/zabbix/api_jsonrpc.php
+	'{"auth":"'${TOKEN_6}'","method":"hostgroup.get","id":1,"params":{"output":"extend"},"jsonrpc":"2.0"}' \
+	http://10.0.3.61/zabbix/api_jsonrpc.php
 echo ""
 
 echo "Machine 7"
 curl -X GET -H "Content-Type:application/json-rpc" -d \
-	'{"auth":"'${TOKEN_2}'","method":"hostgroup.get","id":1,"params":{"output":"extend"},"jsonrpc":"2.0"}' \
-	http://10.0.3.21/zabbix/api_jsonrpc.php
+	'{"auth":"'${TOKEN_7}'","method":"hostgroup.get","id":1,"params":{"output":"extend"},"jsonrpc":"2.0"}' \
+	http://10.0.3.71/zabbix/api_jsonrpc.php
 echo ""
