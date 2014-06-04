08fd4f46fbc32d5c8d45009e5ed73ca706553b48
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2014-06-04 14:13:57 +0900

    [getGroup] Show hosts which is member of Hostgroup.

--- script/for_test/getGroup.sh
+++ script/for_test/getGroup.sh
@@ -6,18 +6,18 @@ TOKEN_7=$3
 
 echo "Machine 2"
 curl -X GET -H "Content-Type:application/json-rpc" -d \
-	'{"auth":"'${TOKEN_2}'","method":"hostgroup.get","id":1,"params":{"output":"extend"},"jsonrpc":"2.0"}' \
+	'{"auth":"'${TOKEN_2}'","method":"hostgroup.get","id":1,"params":{"output":"extend","filter":{"name":["GroupA","GroupB"]},"selectHosts":"refer"},"jsonrpc":"2.0"}' \
 	http://10.0.3.21/zabbix/api_jsonrpc.php
 echo ""
 
 echo "Machine 6"
 curl -X GET -H "Content-Type:application/json-rpc" -d \
-	'{"auth":"'${TOKEN_6}'","method":"hostgroup.get","id":1,"params":{"output":"extend"},"jsonrpc":"2.0"}' \
+	'{"auth":"'${TOKEN_6}'","method":"hostgroup.get","id":1,"params":{"output":"extend","filter":{"name":["GroupA","GroupB"]},"select_hosts":"refer"},"jsonrpc":"2.0"}' \
 	http://10.0.3.61/zabbix/api_jsonrpc.php
 echo ""
 
 echo "Machine 7"
 curl -X GET -H "Content-Type:application/json-rpc" -d \
-	'{"auth":"'${TOKEN_7}'","method":"hostgroup.get","id":1,"params":{"output":"extend"},"jsonrpc":"2.0"}' \
+	'{"auth":"'${TOKEN_7}'","method":"hostgroup.get","id":1,"params":{"output":"extend","filter":{"name":["GroupA","GroupB"]},"selectHosts":"refer"},"jsonrpc":"2.0"}' \
 	http://10.0.3.71/zabbix/api_jsonrpc.php
 echo ""
