2eae2c64aca4d0be013ed1249f1e742d29791637
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2014-06-05 13:07:36 +0900

    [getGroup] Store result to variable value.

--- script/for_test/getGroup.sh
+++ script/for_test/getGroup.sh
@@ -5,19 +5,19 @@ TOKEN_6=$2
 TOKEN_7=$3
 
 echo "Machine 2"
-curl -X GET -H "Content-Type:application/json-rpc" -d \
+ACTUAL_2=$(curl -s -X GET -H "Content-Type:application/json-rpc" -d \
 	'{"auth":"'${TOKEN_2}'","method":"hostgroup.get","id":1,"params":{"output":"extend","filter":{"name":["GroupA","GroupB"]},"selectHosts":"refer"},"jsonrpc":"2.0"}' \
-	http://10.0.3.21/zabbix/api_jsonrpc.php
-echo ""
+	http://10.0.3.21/zabbix/api_jsonrpc.php)
+echo "${ACTUAL_2}"
 
 echo "Machine 6"
-curl -X GET -H "Content-Type:application/json-rpc" -d \
+ACTUAL_6=$(curl -s -X GET -H "Content-Type:application/json-rpc" -d \
 	'{"auth":"'${TOKEN_6}'","method":"hostgroup.get","id":1,"params":{"output":"extend","filter":{"name":["GroupA","GroupB"]},"select_hosts":"refer"},"jsonrpc":"2.0"}' \
-	http://10.0.3.61/zabbix/api_jsonrpc.php
-echo ""
+	http://10.0.3.61/zabbix/api_jsonrpc.php)
+echo "${ACTUAL_6}"
 
 echo "Machine 7"
-curl -X GET -H "Content-Type:application/json-rpc" -d \
+ACTUAL_7=$(curl -s -X GET -H "Content-Type:application/json-rpc" -d \
 	'{"auth":"'${TOKEN_7}'","method":"hostgroup.get","id":1,"params":{"output":"extend","filter":{"name":["GroupA","GroupB"]},"selectHosts":"refer"},"jsonrpc":"2.0"}' \
-	http://10.0.3.71/zabbix/api_jsonrpc.php
-echo ""
+	http://10.0.3.71/zabbix/api_jsonrpc.php)
+echo "${ACTUAL_7}"
