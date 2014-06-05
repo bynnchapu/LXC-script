135936a6fb4c126b89d6642a5a8cce84c1c510a6
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2014-06-05 13:34:50 +0900

    [getGroup] Add compare sequence.

--- script/for_test/getGroup.sh
+++ script/for_test/getGroup.sh
@@ -12,16 +12,34 @@ echo "Machine 2"
 ACTUAL_2=$(curl -s -X GET -H "Content-Type:application/json-rpc" -d \
 	'{"auth":"'${TOKEN_2}'","method":"hostgroup.get","id":1,"params":{"output":"extend","filter":{"name":["GroupA","GroupB"]},"selectHosts":"refer"},"jsonrpc":"2.0"}' \
 	http://10.0.3.21/zabbix/api_jsonrpc.php)
-echo "${ACTUAL_2}"
+if [ ${ACTUAL_2} = ${EXPECT_2} ];
+then
+	echo "OK"
+else
+	echo "NG"
+	echo "${ACTUAL_2}"
+fi
 
 echo "Machine 6"
 ACTUAL_6=$(curl -s -X GET -H "Content-Type:application/json-rpc" -d \
 	'{"auth":"'${TOKEN_6}'","method":"hostgroup.get","id":1,"params":{"output":"extend","filter":{"name":["GroupA","GroupB"]},"select_hosts":"refer"},"jsonrpc":"2.0"}' \
 	http://10.0.3.61/zabbix/api_jsonrpc.php)
-echo "${ACTUAL_6}"
+if [ ${ACTUAL_6} = ${EXPECT_6} ];
+then
+	echo "OK"
+else
+	echo "NG"
+	echo "${ACTUAL_6}"
+fi
 
 echo "Machine 7"
 ACTUAL_7=$(curl -s -X GET -H "Content-Type:application/json-rpc" -d \
 	'{"auth":"'${TOKEN_7}'","method":"hostgroup.get","id":1,"params":{"output":"extend","filter":{"name":["GroupA","GroupB"]},"selectHosts":"refer"},"jsonrpc":"2.0"}' \
 	http://10.0.3.71/zabbix/api_jsonrpc.php)
-echo "${ACTUAL_7}"
+if [ ${ACTUAL_7} = ${EXPECT_7} ];
+then
+	echo "OK"
+else
+	echo "NG"
+	echo "${ACTUAL_7}"
+fi
